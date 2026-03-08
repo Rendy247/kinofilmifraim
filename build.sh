706474d114b2a9a1822149689f58108ae3727018
Rendy247 <rasulgatin08@gmail.com>
2026-03-08 11:45:45 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Устанавливаем Playwright + Chromium (именно так требует Leapcell)
-npx -y playwright@1.50.1 install --with-deps chromium
-# Устанавливаем зависимости проекта
 npm install
+npx playwright install chromium
+npx playwright install-deps chromium
